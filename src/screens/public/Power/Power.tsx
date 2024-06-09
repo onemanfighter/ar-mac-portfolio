7ada6115247c339bb166830a7574d5b28dd667a7
Amit Raikwar | OMF <149316922+onemanfighter@users.noreply.github.com>
2024-06-10 03:36:23 +0530

    feat(): Adding full screen help text in the main power screenwq (#38)

--- src/screens/public/Power/Power.tsx
+++ src/screens/public/Power/Power.tsx
@@ -1,14 +1,17 @@
-import { Box, IconButton } from '@chakra-ui/react';
+import { Box, IconButton, Text } from '@chakra-ui/react';
 import { MacIcon, PowerIcon } from '@assets';
 import { PowerProps } from './type';
 import { processStore, powerSelector, useShallow } from '@processStore';
+import { useTranslation } from 'react-i18next';
+import _ from 'lodash';
 
 const Power = (props: PowerProps) => {
+  const { t } = useTranslation();
   const { poweringOn } = processStore(useShallow(powerSelector));
-
   const powerClickHandler = () => {
     poweringOn();
   };
+
   return (
     <Box
       bg={'black'}
@@ -16,7 +19,7 @@ const Power = (props: PowerProps) => {
       flex={1}
       alignItems={'center'}
       height={'100vh'}
-      rowGap={100}
+      rowGap={50}
       aria-label="power-screen"
       justifyContent={'center'}
       flexDirection={'column'}
@@ -34,6 +37,38 @@ const Power = (props: PowerProps) => {
           onClick={powerClickHandler}
           size="lg"
         />
+      </Box>
+      <Box
+        color={'white'}
+        display={'flex'}
+        flexDir={'row'}
+        width={'100%'}
+        maxHeight={5}
+        justifyContent={'center'}
+        alignItems={'center'}
+        alignContent={'center'}
+      >
+        {_.toArray(t('PowerScreen.helpText')).map((character) => {
+          return (
+            <Text
+              fontFamily={'mono'}
+              key={character}
+              _hover={{
+                color: 'black',
+                bg: 'white',
+                fontFamily: 'mono',
+                borderRadius: 'full',
+                opacity: 0.8,
+                padding: 2,
+                fontSize: '2xl',
+                transition: 'all 0s ease ',
+                cursor: 'pointer',
+              }}
+            >
+              {character === ' ' ? <>&nbsp;</> : character}
+            </Text>
+          );
+        })}
       </Box>
     </Box>
   );
