7c68bfe6303bab44e7c707a112e46e5ee8dcab14
Amit Raikwar <managerarbusiness@gmail.com>
2024-06-10 02:08:52 +0530

    feat(): Adding full-screen shortcut

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-
 import {
   ChakraUIProvider,
   ModalProvider,
   RouterProviderComponent,
 } from '@providers';
+import { useEffect } from 'react';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
 
 function App() {
-  const { t } = useTranslation();
+  const handle = useFullScreenHandle();
+
+  const handleKeyPress = (event: any) => {
+    switch (event.key) {
+      case 'F10':
+        handle.enter();
+        break;
+      case 'Escape':
+        handle.exit();
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <ChakraUIProvider>
-      <ModalProvider>
-        <RouterProviderComponent />
-      </ModalProvider>
+      <FullScreen handle={handle}>
+        <ModalProvider>
+          <RouterProviderComponent />
+        </ModalProvider>
+      </FullScreen>
     </ChakraUIProvider>
   );
 }
